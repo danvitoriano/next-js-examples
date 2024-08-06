e8fc45e0767e633c403712bd99ff8693b9c3b0a9
Dan Vitoriano <vitoriano08@gmail.com>
2024-08-06 16:18:36 -0300

    api

--- solutions/express/api/index.ts
+++ solutions/express/api/index.ts
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 // Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 app.use(express.static('public'));
 
@@ -25,13 +25,8 @@ app.get('/uploadUser', function (req, res) {
 });
 
 app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
-	try {
-		await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
-		res.status(200).send('<h1>User added successfully</h1>');
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error adding user');
-	}
+	console.log(req.body.email);
+    res.status(200).send('User uploaded successfully');
 });
 
 app.get('/allUsers', async (req, res) => {
@@ -107,6 +102,6 @@ app.get('/allUsers', async (req, res) => {
 	}
 });
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+app.listen(3003, () => console.log('Server ready on port 3000.'));
 
 module.exports = app;
